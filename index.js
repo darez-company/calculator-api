0887e5d0a701d1e61e35c325d7e2e4cd9052c17e
Gabriel Bolzi <gabriel03899@hotmail.com>
2020-12-14 17:45:27 -0300

    test ccron

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const http = require('./server')
 const database = require('./databaseBootstrap')
+const cron = require('node-cron')
+const { sendMetricsEmail } = require('./controllers/metricsController')
 
 async function startServer() {
  try {
@@ -13,5 +15,13 @@ async function startServer() {
     process.exit(0)
  }
 }
+
+cron.schedule('49 17 * * *', () => {
+   sendMetricsEmail({ params: { companyID
+   : '560'} }, { status: (param) => ({ end: (param) => null }) })
+}, {
+   scheduled: true,
+   timezone: "America/Sao_Paulo"
+ })
 
 startServer()
