4f34aa9fbe5e6c547d689d15177a185c7b969264
Gabriel Bolzi <gabriel03899@hotmail.com>
2020-12-14 17:03:58 -0300

    add

--- controllers/metricsController.js
+++ controllers/metricsController.js
@@ -30,11 +30,12 @@ const sendMetricsEmail = async ({ params: { companyID }, query: { queryDate } },
         const entries = await calculatorEntries.findOne({ userCompanyID: companyID, date: queryDate || date }).exec()
         if (!entries || !entries.entries) return res.status(204).end()
         const excelName = await generateExcel(entries.entries, queryDate || date)
-        await sendEmail({ excelName, companyID })
+        sendEmail({ excelName, companyID })
         await sleep(1000)
         fs.unlink(`${__dirname}/../${excelName}`, err => {
             if (err) console.error(err)
         })
+        return res.status(200).end()
     } catch (err) {
         console.error(err)
         res.status(500)
